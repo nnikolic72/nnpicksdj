59cbd7d716f6758fb7bde93065351c8a4fa9332d
Nenad Nikolic <nnenad@gmail.com>
2015-02-11 20:45:34 +0100

    New test

--- goodusers/models.py
+++ goodusers/models.py
@@ -6,7 +6,7 @@ from django.template.defaultfilters import default
 # Create your models here.
 class GoodUser(models.Model):
     '''Data model class for Good User, handpicked Instagram user'''
-    '''Test GitHub from home PC'''
+    '''Test GitHub from home PC, second try'''
     
     def xyz(self):
         return None
